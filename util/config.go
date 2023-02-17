8dc98dcaeddd8fc19cae2f98a851223923e13bcd
yinghou <yinghouuu@gmail.com>
2023-02-17 02:14:18 -0600

    bank_api

---
+++ util/config.go
@@ -0,0 +1,36 @@
+package util
+
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+// Config stores all configuration of the application.
+// The values are read by viper from a config file or environment variable.
+type Config struct {
+	DBDriver            string        `mapstructure:"DB_DRIVER"`
+	DBSource            string        `mapstructure:"DB_SOURCE"`
+	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
+	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+}
+
+// viper uses mapstructure under the hood for unmarshaling values
+
+// LoadConfig reads configuration from file or environment variables.
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv() // override the values from config file with values of corresponding environment variables if they exist
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
